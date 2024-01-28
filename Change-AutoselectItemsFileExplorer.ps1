d1282434765d39557d051a9dc32e7ab77e4cc8aa
alexzout <43434608+alexzout@users.noreply.github.com>
2024-01-28 15:06:26 +0100

    Add files via upload

---
+++ Change-AutoselectItemsFileExplorer.ps1
@@ -0,0 +1,10 @@
+﻿# Set variables to indicate value and key to set
+$RegistryPath = 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced'
+$Name         = 'AutoCheckSelect'
+$Value        = '0'
+# Create the key if it does not exist
+If (-NOT (Test-Path $RegistryPath)) {
+  New-Item -Path $RegistryPath -Force | Out-Null
+}  
+# Now set the value
+New-ItemProperty -Path $RegistryPath -Name $Name -Value $Value -PropertyType DWORD -Force 
